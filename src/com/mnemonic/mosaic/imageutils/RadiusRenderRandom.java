c597cab71c94b819469de26f1509d9f301fd3395
fredimnemonic <fredimnemonic@gmail.com>
2010-12-30 09:16:33 +0100

    next

--- src/com/mnemonic/mosaic/imageutils/RadiusRenderRandom.java
+++ src/com/mnemonic/mosaic/imageutils/RadiusRenderRandom.java
@@ -53,8 +53,8 @@ public class RadiusRenderRandom {
     orig.getPixels(pixels, 0, width, 0, 0, width, height);
 
     int currenttilex = 0;
-    int currenttiley = 0;
     for (int x = 0; currenttilex < tilecount ; x += tilex, currenttilex++) {
+      int currenttiley = 0;
       for (int y = 0; currenttiley < tilecount; y += tiley, currenttiley++) {
         double[] mean = new double[3];
 
@@ -75,6 +75,11 @@ public class RadiusRenderRandom {
 /**********************************************************************/
 
         colors[currenttilex][currenttiley] = Color.argb(255, (int) mean[0], (int) mean[1], (int) mean[2]);
+        mean[0] = 0;
+        mean[1] = 0;
+        mean[2] = 0;
+
+        System.out.println("Color " + currenttilex + " " + currenttiley + "  -  " + colors[currenttilex][currenttiley]);
 
 //          int color = Color.argb(255, (int) mean[0], (int) mean[1], (int) mean[2]);
 //          int tileindex = findBestFit(images, color, tileArray, x, y, tilecount);
@@ -103,9 +108,10 @@ public class RadiusRenderRandom {
     System.out.println("Done reading colors! Time -> " + (System.currentTimeMillis() - start));
     start = System.currentTimeMillis();
 
+
+
     for (int x = 0; x < tilecount; x++) {
       for (int y = 0; y < tilecount; y++) {
-//        int tileindex = 3;
         int tileindex = findBestFit(images, colors[x][y], tileArray, x, y, tilecount);
         Bitmap origtile = BitmapFactory.decodeFile(images.get(tileindex).getFilePath());
         Bitmap tile = Bitmap.createScaledBitmap(origtile, tilex, tiley, false);
@@ -136,15 +142,15 @@ public class RadiusRenderRandom {
     int size = tileLibrary.size();
     for (int count = 0; count < size; count++) {  // Cycle through all of the library tiles.
       int imagecolor = tileLibrary.get(count).getColor();
-//      if ( !TileChecker.checkPlacement(1, 0, tileArray, x, y, count, tilecount, tilecount) ) {// If this tile isn't in the box, find the difference in color.
-      redDiff = Math.abs(red - Color.red(imagecolor));
-      blueDiff = Math.abs(blue - Color.blue(imagecolor));
-      greenDiff = Math.abs(green - Color.green(imagecolor));
-      if (((redDiff + blueDiff + greenDiff) < totalDiff)) { // if this is closer than the previous closest...
-        totalDiff = redDiff + blueDiff + greenDiff;
-        closestSoFar = count;  // Keep track of this tile.
+      if ( !TileChecker.checkPlacement(1, 0, tileArray, x, y, count, tilecount, tilecount) ) {// If this tile isn't in the box, find the difference in color.
+        redDiff = Math.abs(red - Color.red(imagecolor));
+        blueDiff = Math.abs(blue - Color.blue(imagecolor));
+        greenDiff = Math.abs(green - Color.green(imagecolor));
+        if (((redDiff + blueDiff + greenDiff) < totalDiff)) { // if this is closer than the previous closest...
+          totalDiff = redDiff + blueDiff + greenDiff;
+          closestSoFar = count;  // Keep track of this tile.
+        }
       }
-//      }
     }
     return closestSoFar;  // return the tile we chose.
   }
